2ddb4b67cd29486a6a6dee94ea95e151a48a1510
Sah test <sahar-ilya@mail.ru>
2025-01-29 16:14:33 +0700

    Miney Transfer

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,10 @@
 package ru.netology
 
 fun main () {
-    val amount: Int = 1000
-    val 
+    val amount: Int = 1000000
+    val perCommission: Float = 0.75F
+    val limit: Int = 35
+    val totalCommission: Float = amount * perCommission / 100
+    val result = if (totalCommission < limit) limit else totalCommission
+    println("Комиссия составит: $result рублей")
 }
